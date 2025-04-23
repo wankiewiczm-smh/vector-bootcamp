22d2245f6c301a869c71518a6dd3aceefd3b3624
20danik <90417495+20danik@users.noreply.github.com>
2025-04-23 12:47:53 -0400

    Added file to clean up data by: using bin centre value for age, drop missing gender, binarize gender into female, assign ? race to Other.

--- cleaning.py
+++ cleaning.py
@@ -17,8 +17,30 @@ def load_and_clean(df):
     ## remove unnecessary columns
     df_clean = df.drop(['weight', 'encounter_id', 'patient_nbr', 'medical_specialty', 'payer_code', 'readmitted'], axis = 1)
 
+    ## transform ages
+    age_transform = {'[0-10)': 5,
+                     '[10-20)': 15,
+                     '[20-30)': 25,
+                     '[30-40)': 35,
+                     '[40-50)': 45,
+                     '[50-60)': 55,
+                     '[60-70)': 65,
+                     '[70-80)': 75,
+                     '[80-90)': 85,
+                     '[90-100)': 95
+                     }
+    df_clean['age'] = df_clean['age'].apply(lambda x: age_transform[x])
+
+    ## Drop missing gender
+    df_clean = df_clean.dropna(subset=['gender'])
+
+    ## binarize gender as 1 = Female 0 = Male
+    df_clean['female'] = df_clean['gender'].apply(
+        lambda x: 1 if x == 'Female' else 0)
+
+    ## Assign ? Race with "Other"
+    df_clean['race'] = df_clean['race'].apply(lambda x: "Other" if x == "?" else x)
+
     ## return the table
     return(df_clean)
-
-
 
