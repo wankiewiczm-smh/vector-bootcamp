852d90bbf6a5d22949256af1f03d75e8db84af99
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2025-04-23 13:01:51 -0400

    update cleaning function from df_clean to df

--- cleaning.py
+++ cleaning.py
@@ -9,13 +9,13 @@ def load_and_clean(df):
     """
 
     ## create total_previous_visits
-    df_clean['total_previous_visits'] = df_clean['number_outpatient'] + df_clean['number_inpatient'] + df_clean['number_emergency']
+    df['total_previous_visits'] = df['number_outpatient'] + df['number_inpatient'] + df['number_emergency']
 
     ## create outcome column, convert to binary
-    df_clean = (df_clean['readmitted'] == '<30').astype(int)
+    df = (df['readmitted'] == '<30').astype(int)
 
     ## remove unnecessary columns
-    df_clean = df.drop(['weight', 'encounter_id', 'patient_nbr', 'medical_specialty', 'payer_code', 'readmitted'], axis = 1)
+    df = df.drop(['weight', 'encounter_id', 'patient_nbr', 'medical_specialty', 'payer_code', 'readmitted'], axis = 1)
 
     ## transform ages
     age_transform = {'[0-10)': 5,
@@ -29,18 +29,18 @@ def load_and_clean(df):
                      '[80-90)': 85,
                      '[90-100)': 95
                      }
-    df_clean['age'] = df_clean['age'].apply(lambda x: age_transform[x])
+    df['age'] = df['age'].apply(lambda x: age_transform[x])
 
     ## Drop missing gender
-    df_clean = df_clean.dropna(subset=['gender'])
+    df = df.dropna(subset=['gender'])
 
     ## binarize gender as 1 = Female 0 = Male
-    df_clean['female'] = df_clean['gender'].apply(
+    df['female'] = df['gender'].apply(
         lambda x: 1 if x == 'Female' else 0)
 
     ## Assign ? Race with "Other"
-    df_clean['race'] = df_clean['race'].apply(lambda x: "Other" if x == "?" else x)
+    df['race'] = df['race'].apply(lambda x: "Other" if x == "?" else x)
 
     ## return the table
-    return(df_clean)
+    return(df)
 
